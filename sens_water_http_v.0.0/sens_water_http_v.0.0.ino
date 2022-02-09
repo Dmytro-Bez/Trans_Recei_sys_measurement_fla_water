3d45daf9d61118c5da4bc95e31bb574420367017
Dmytriy <61518737+Dmytro-Bez@users.noreply.github.com>
2022-02-09 16:07:24 +0200

    Added functions

--- sens_water_http_v.0.0/sens_water_http_v.0.0.ino
+++ sens_water_http_v.0.0/sens_water_http_v.0.0.ino
@@ -2,26 +2,32 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
-#define SENSOR 0
-
 int sens_val;
 unsigned long currentMillis = 0;                          //Create variable mil.sec
 unsigned long previousMillis = 0;
-const long interval = 2000;  
-     
+const long interval = 2000; 
+RF24 radio(7, 8); // CE, CSN
+
+const byte address[6] = "00001";
+
 void setup() {
   Serial.begin(115200);
-
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
+  Serial.println("Start nrf");
 }
 
 void loop() {
-  currentMillis = millis();                               //Call delay functions
+currentMillis = millis();                               //Call delay functions
   if(currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
     sens_val = analogRead(0);
     if(sens_val >= 0 && sens_val <= 800){
       Serial.print("Water %=");
       Serial.println(sens_val);
+      radio.write(&sens_val, sizeof(sens_val));
     }
   }
 }
--- server_http_v.0.0/server_http_v.0.0.ino
+++ server_http_v.0.0/server_http_v.0.0.ino
@@ -1,34 +1,14 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
 
-const char* ssid = "EE";                        //Login
-const char* password = "EE@05kilogram";         //Password
 
-void setup() {
-  Serial.begin(115200);
-  delay(500);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to Wi-Fi..");
-  }
-  Serial.println("Not connecting Wi-Fi!");
-}
-
-void loop() {
-  if ((WiFi.status() == WL_CONNECTED)){                             //Check to connect for the wifi
-    HTTPClient http;                                                //Creating object HTTP
-    http.begin("http://*");     //Connecting to the web
-    int httpCode = http.GET();                                      //Ping GET
-    if (httpCode > 0){                                              //Check                               
-      String payload = http.getString();                            //Display messeger web
-      Serial.println(httpCode);
-      Serial.println(payload);
-    }
-    else {
-      Serial.println("Error HTTP-get");
-    }
-    http.end();                                                     //Disconnect web
-  }
-  delay(10000);
-}
+//  WiFi.begin(ssid, password);
+//  if(WiFi.status() != WL_CONNECTED) {
+//    delay(1000);
+//    Serial.println("Connecting to Wi-Fi..");
+//  } else {
+//    Serial.println("Not connecting Wi-Fi!");
+//  }
